c382e89b056f7342c436dee9d8cd286bf27cf37c
Wellington Oliveira <well.oliveira.snt@gmail.com>
2019-01-31 15:54:31 -0300

    Removing blink on select option

--- src/Select/SelectContainer.js
+++ src/Select/SelectContainer.js
@@ -2,40 +2,45 @@ import React from 'react';
 import { components } from 'react-select';
 import { withStyles } from '@material-ui/core/styles';
 
-const withStylesProps = styles => Component => props => {
-  const Comp = withStyles(theme => styles(theme, props))(Component);
-  return <Comp {...props} />;
+const style = {
+  divStyledSelect: {
+    height: 47,
+    '&:focus': { outline: 'none !important' },
+  },
 };
 
-const divStyles = (theme, { selectProps }) => {
-  let color = 'rgba(0, 0, 0, 0.42)';
-  let borderStyle = 'solid';
-  let size = '1px';
-  if (selectProps.isDisabled) {
-    borderStyle = 'dotted';
+export const SelectContainer = withStyles(style)(
+  ({ children, selectProps, classes, ...props }) => {
+    let color = 'rgba(0, 0, 0, 0.42)';
+    let borderStyle = 'solid';
+    let size = '1px';
+
+    if (selectProps.isDisabled) {
+      borderStyle = 'dotted';
+    }
+
+    if (selectProps.meta.error && selectProps.meta.touched) {
+      color = 'red';
+      size = '2px';
+    }
+
+    const styleDiv = {
+      divStyled: {
+        borderBottom: `${size} ${borderStyle} ${color}`,
+      },
+    };
+
+    return (
+      <div
+        style={styleDiv.divStyled}
+        name={selectProps.name}
+        className={classes.divStyledSelect}
+        tabIndex={Math.floor(Math.random() * 80)}
+      >
+        <components.SelectContainer {...props}>
+          {children}
+        </components.SelectContainer>
+      </div>
+    );
   }
-  if (selectProps.meta.error && selectProps.meta.touched) {
-    color = 'red';
-    size = '2px';
-  }
-  return {
-    divStyled: {
-      height: 47,
-      borderBottom: `${size} ${borderStyle} ${color}`,
-      '&:focus': { outline: 'none !important' },
-    },
-  };
-};
-export const SelectContainer = withStylesProps(divStyles)(
-  ({ children, selectProps, classes, ...props }) => (
-    <div
-      className={classes.divStyled}
-      name={selectProps.name}
-      tabIndex={Math.floor(Math.random() * 80)}
-    >
-      <components.SelectContainer {...props}>
-        {children}
-      </components.SelectContainer>
-    </div>
-  )
 );
