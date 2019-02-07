0daeb8e74604d08a0404fecab9db1dc6adb4d37e
Wellington Oliveira <well.oliveira.snt@gmail.com>
2019-02-07 17:54:24 -0300

    Added huskyy

--- src/Select/Controlnput.js
+++ src/Select/Controlnput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, } from '@material-ui/styles';
 import classNames from 'classnames';
 import { TextFieldComponent } from '../Inputs/Input';
 
