7db0428140e5d7ac1d39b55ed41d2bee73aa7d71
Wellington Oliveira <well.oliveira.snt@gmail.com>
2019-02-08 17:17:05 -0300

    Showing items lenght at multi select

--- src/Select/ValueContainer.js
+++ src/Select/ValueContainer.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { components } from 'react-select';
 
-export function ValueContainer({ selectProps, children }) {
+export function ValueContainer({ selectProps, children, getValue, ...props }) {
+  const { length } = getValue();
+
+  if (selectProps.isMulti) {
+    return (
+      <components.ValueContainer {...props}>
+        {!selectProps.menuIsOpen &&
+          `${length} Item${length !== 1 ? 's' : ''} selecionados`}
+        {React.cloneElement(children[1])}
+      </components.ValueContainer>
+    );
+  }
+
   return (
     <div className={selectProps.childrenClasses.valueContainer}>
       <div>{children}</div>
