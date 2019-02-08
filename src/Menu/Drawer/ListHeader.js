e596f3367eabf10af6ea0d3fc8cd8c58ae502b21
Renan Castro <renanccastro@gmail.com>
2019-02-08 14:22:57 -0200

    Search Bar

--- src/Menu/Drawer/ListHeader.js
+++ src/Menu/Drawer/ListHeader.js
@@ -5,10 +5,13 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import { mdiTurtle } from '@mdi/js';
 import Icon from '@mdi/react';
+import classNames from 'classnames';
 
+import SearchBar from 'material-ui-search-bar';
+import { grey } from '@material-ui/core/colors';
 import { DefaultProductTypography } from '../DefaultProductTypography';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(({ spacing }) => ({
   flexColumn: {
     display: 'flex',
     flexDirection: 'column',
@@ -16,7 +19,17 @@ const useStyles = makeStyles({
   noPadding: {
     padding: 0,
   },
-});
+  searchBar: {
+    flexGrow: '1',
+    paddingRight: spacing.unit,
+    boxShadow: 'none',
+    width: `calc(100% - ${spacing.unit}px + 6px )`,
+    borderRadius: 0,
+    backgroundColor: grey[200],
+    paddingTop: spacing.unit / 2,
+    paddingBottom: spacing.unit / 2,
+  },
+}));
 
 export const ListHeader = () => {
   const classes = useStyles();
@@ -31,6 +44,16 @@ export const ListHeader = () => {
         <ListItemSecondaryAction>
           <Icon path={mdiTurtle} size={1} />
         </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem
+        alignItems="flex-start"
+        divider
+        className={classNames(classes.flexColumn, classes.noPadding)}
+      >
+        <SearchBar
+          placeholder="O que você busca?"
+          className={classes.searchBar}
+        />
       </ListItem>
     </Fragment>
   );
