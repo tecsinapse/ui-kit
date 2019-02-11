58b1fea8c2b413f837202a7ded0db91b2deadcee
Renan Castro <renanccastro@gmail.com>
2019-02-11 13:48:19 -0200

    adding travis integration

--- src/Menu/Drawer/searchLogic.js
+++ src/Menu/Drawer/searchLogic.js
@@ -3,7 +3,11 @@ import { flatten } from '@tecsinapse/es-utils/core/object/index';
 export function searchLogic(items, searchText, subtitle = null) {
   const found = [];
   for (const item of items) {
-    if (
+    if (item.children) {
+      found.push(
+        searchLogic(item.children, searchText, subtitle || item.title)
+      );
+    } else if (
       item.title.toLowerCase().indexOf(searchText.toLowerCase()) >= 0 &&
       subtitle
     ) {
@@ -11,10 +15,6 @@ export function searchLogic(items, searchText, subtitle = null) {
         title: item.title,
         subtitle,
       });
-    } else if (item.children) {
-      found.push(
-        searchLogic(item.children, searchText, subtitle || item.title)
-      );
     }
   }
   return flatten(found);
