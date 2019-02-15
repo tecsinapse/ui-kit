5e439a2d1d84d42fd07db7f9eb56c4cbe037f89f
Jesse Alves <jessecarlos@gmail.com>
2019-02-15 18:13:11 -0200

    Initial commit to create a Table component

---
+++ src/Table/Table.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import MUITable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  table: {
+    width: '100%',
+  },
+}));
+
+const objectDeepPath = (obj, key) => {
+  if (!key || !obj) return obj;
+
+  const fieldNames = key.split('.');
+
+  let value = { ...obj };
+
+  fieldNames.forEach(field => {
+    if (value == null || value === undefined) return;
+    value = value[field];
+  });
+
+  return typeof value === 'object' ? JSON.stringify(value) : value;
+};
+
+const convertColumnToTableCell = ({ title, options = {} }) => (
+  <TableCell align={options.numeric ? 'right' : 'left'}>{title}</TableCell>
+);
+
+const createHeaders = columns => {
+  let tableCells = null;
+
+  if (columns && columns.length > 0) {
+    tableCells = columns.map(convertColumnToTableCell);
+  }
+
+  return (
+    <TableHead>
+      <TableRow>{tableCells}</TableRow>
+    </TableHead>
+  );
+};
+
+const convertValuesToTableCell = ({ field, options = {} }, values) => (
+  <TableCell align={options.numeric ? 'right' : 'left'}>
+    {objectDeepPath(values, field)}
+  </TableCell>
+);
+
+const convertDataValuesToTableRow = (columns, dataValues) => (
+  <TableRow>
+    {columns.map(column => convertValuesToTableCell(column, dataValues))}
+  </TableRow>
+);
+
+const createBody = (columns, data) => {
+  let rows = null;
+
+  if (columns && columns.length > 0 && data && data.length > 0) {
+    rows = data.map(dataValues =>
+      convertDataValuesToTableRow(columns, dataValues)
+    );
+  }
+
+  return <TableBody>{rows}</TableBody>;
+};
+
+const Table = props => {
+  const { columns, data } = props;
+  const classes = useStyles();
+  return (
+    <MUITable className={classes.table}>
+      {createHeaders(columns)}
+      {createBody(columns, data)}
+    </MUITable>
+  );
+};
+
+export default Table;
