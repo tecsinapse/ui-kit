137341a486e130be5fc45bdcd93147b0c176caee
Felipe <felipefuty01@gmail.com>
2019-03-14 20:33:01 -0600

    Implement preview list

--- src/UploadFile/PreviewList.js
+++ src/UploadFile/PreviewList.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
 
-export const PreviewList = ({ value }) => (
-  <ul>
-    <li>File1</li>
-    <li>File2</li>
-  </ul>
-);
+function PreviewList({ value }) {
+  return (
+    <Grid container spacing={8}>
+      {value.map(fileObject => (
+        <Grid item xs={4} key={fileObject._id}>
+          <p>{fileObject.file.name}</p>
+          <LinearProgress variant="determinate" value={fileObject.completed} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
 
 export default PreviewList;
