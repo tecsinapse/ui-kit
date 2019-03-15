743cc79edcc0388d3f629b8b861f34398f47ad92
Felipe <felipefuty01@gmail.com>
2019-03-14 20:09:48 -0600

    Implement uploader

--- src/UploadFile/Uploader.js
+++ src/UploadFile/Uploader.js
@@ -1,33 +1,43 @@
 import React, { Fragment } from 'react';
-import Dropzone from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 import PropTypes from 'prop-types';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { PreviewList } from './PreviewList';
 
 function Uploader({
   value,
-  acceptedFiles,
+  acceptedFormat,
   filesLimit,
   maxFileSize,
   dropzoneText,
   onChange,
 }) {
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: acceptedFormat.join(','),
+    maxSize: maxFileSize,
+    onDrop: acceptedFiles => {
+      if (acceptedFiles.length + value.length < filesLimit && onChange) {
+        onChange(acceptedFiles);
+      }
+      // TODO (LIP): Define what it should happen for ignored files
+    },
+  });
+
   return (
     <Fragment>
-      <Dropzone>
-        <div>
-          <p> dropzoneText </p>
-          <CloudUploadIcon />
-        </div>
-      </Dropzone>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p> {dropzoneText} </p>
+        <CloudUploadIcon />
+      </div>
       <PreviewList value={value} />
     </Fragment>
   );
 }
 
 Uploader.defaultProps = {
-  value: null,
-  acceptedFiles: ['image/*', 'video/*', 'application/*'],
+  value: [],
+  acceptedFormat: ['image/*', 'video/*', 'application/*'],
   filesLimit: 3,
   maxFileSize: 3000000,
   dropzoneText: 'Drag and drop an image file here or click',
@@ -35,16 +45,18 @@ Uploader.defaultProps = {
 };
 
 Uploader.propTypes = {
-  acceptedFiles: PropTypes.array,
+  acceptedFormat: PropTypes.array,
   filesLimit: PropTypes.number,
   maxFileSize: PropTypes.number,
   dropzoneText: PropTypes.string,
   onChange: PropTypes.func,
-  value: PropTypes.shape({
-    _id: PropTypes.number,
-    file: PropTypes.object,
-    upProgress: PropTypes.number,
-  }),
+  value: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.number,
+      file: PropTypes.object,
+      upProgress: PropTypes.number,
+    })
+  ),
 };
 
 export default Uploader;
