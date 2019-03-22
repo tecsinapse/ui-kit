17ddddded7088fcac4ac93672cb6acb64e471aa8
Jesse Alves <jessecarlos@gmail.com>
2019-03-22 17:43:19 -0300

    Adding graphql-hooks to table server side example

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,8 +6,12 @@ import { setDefaults } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 import { setOptions } from '@storybook/addon-options';
 import { withInfo } from '@storybook/addon-info';
-
 import { withSmartKnobs } from 'storybook-addon-smart-knobs';
+import { GraphQLClient, ClientContext } from 'graphql-hooks'
+
+const client = new GraphQLClient({
+	url: 'https://countries.trevorblades.com/'
+  });
 
 setOptions({
   hierarchySeparator: /\//,
@@ -15,6 +19,10 @@ setOptions({
   name: 'TecSinapse UI-KIT',
   url: 'https://github.com/tecsinapse/ui-kit',
 });
+
+const withGraphqlClientProvider = storyFn => (
+	<ClientContext.Provider value={client}>{storyFn()}</ClientContext.Provider>
+);
 
 const withThemeProvider = storyFn => (
   <ThemeProvider variant="orange">{storyFn()}</ThemeProvider>
@@ -46,6 +54,7 @@ function loadStories() {
   addDecorator(withInfo);
   addDecorator(withStoryStyles);
   addDecorator(withThemeProvider);
+  addDecorator(withGraphqlClientProvider);
   req.keys().forEach(filename => req(filename));
 }
 
