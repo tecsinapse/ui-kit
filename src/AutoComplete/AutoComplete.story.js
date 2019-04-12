282480143effd01fc94eb0f21d0e18790cbd3bfe
Jesse Alves <jessecarlos@gmail.com>
2019-04-12 16:15:50 -0300

    [#114910] Criar componente de autocomplete

--- src/AutoComplete/AutoComplete.story.js
+++ src/AutoComplete/AutoComplete.story.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
+import { useManualQuery } from 'graphql-hooks';
 import { GROUPS } from '../../.storybook/hierarchySeparators';
 import AutoComplete from './AutoComplete';
 
+const CONTINENTS_QUERY = `query continents {
+	continents {
+		code
+	  name
+	}
+  }`;
+
+const options = fetch => async inputValue => {
+  const {
+    data: { continents },
+  } = await fetch();
+  return continents
+    .map(continent => ({
+      id: continent.code,
+      label: continent.name,
+    }))
+    .filter(continent =>
+      continent.label.toLowerCase().startsWith(inputValue.toLowerCase())
+    );
+};
+
+const AutoCompleteExample = () => {
+  const [values, setValues] = useState([]);
+  const [fetch] = useManualQuery(CONTINENTS_QUERY);
+
+  return (
+    <div style={{ width: '300px' }}>
+      <AutoComplete
+        options={options(fetch)}
+        values={values}
+        onDeleteItem={value =>
+          setValues(oldValues => oldValues.filter(v => v.id !== value.id))
+        }
+        onSelectItem={suggestion =>
+          setValues(oldValues => [...oldValues, suggestion])
+        }
+        inputProps={{
+          label: 'Search for continents',
+        }}
+      />
+    </div>
+  );
+};
+
 storiesOf(`${GROUPS.FORMS}|Autocomplete`, module).add('Autocomplete', () => (
-  <AutoComplete />
+  <AutoCompleteExample />
 ));
