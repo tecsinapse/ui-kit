2f1d4f16b67041513f974459a2d2f7723361d224
Felipe <felipefuty01@gmail.com>
2019-04-15 09:58:26 -0500

    Remove useless example

--- src/UploadFile/Uploader.story.js
+++ src/UploadFile/Uploader.story.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import List from '@material-ui/core/List';
 import { storiesOf } from '@storybook/react';
 import { TesteUploader } from './TesteUploader';
-import { UpFile } from './UpFile';
 
 storiesOf(`Uploader`, module)
   .add('uploader drag', () => (
     <div style={{ width: '700px', height: '400px' }}>
       <TesteUploader />
-    </div>
-  ))
-  .add('list example', () => (
-    <div style={{ width: '450px', height: '300px' }}>
-      <List>
-        <UpFile
-          filename="bla111111111111111111111111111111.png"
-          completed={99}
-          filesize="10MB"
-          uprate="1Mb/s"
-        />
-        <UpFile
-          filename="bla111111111111111111.png"
-          completed={100}
-          filesize="10MB"
-          uprate="1Mb/s"
-          data="https://weeblytutorials.com/wp-content/uploads/2017/04/Weebly-blogs-example.png"
-        />
-        <UpFile
-          filename="bla2.png"
-          completed={10}
-          filesize="10MB"
-          uprate="1Mb/s"
-          error="File type not supported"
-        />
-      </List>
     </div>
   ))
   .add('uploader form', () => (
