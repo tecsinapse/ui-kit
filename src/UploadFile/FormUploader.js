45e34da9a204a0c3c5d03755db465931c3cdd5b0
Renan Castro <renanccastro@gmail.com>
2019-04-17 19:27:24 -0300

    typo

--- src/UploadFile/FormUploader.js
+++ src/UploadFile/FormUploader.js
@@ -11,7 +11,6 @@ const useStyle = makeStyles(theme => ({
     display: 'flex',
     width: '100%',
     height: '100%',
-    overwride: 'hidden',
   },
   rootmobile: {
     flexDirection: 'column',
