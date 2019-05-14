5cc6ebd9d066db7b3f25d33eda93be24bbcbd455
Renan Castro <renanccastro@gmail.com>
2019-05-14 19:16:11 -0300

    Revert "Clickaway to fix close on react-select"
    This reverts commit 5769c324

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -8,8 +8,6 @@ import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMe
 import PropTypes from 'prop-types';
 import ReactSelect from 'react-select';
 import { Help } from '@material-ui/icons';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-
 import { SizeMe } from 'react-sizeme';
 import { selectInputStyle } from './SelectInputStyle';
 import { SelectMobileCustomComponents } from './SelectMobileCustomComponents';
@@ -141,6 +139,7 @@ export const SelectUnstyled = ({
       if (onBlur) {
         onBlur(event);
       }
+      selectProps.setMenuIsOpen(false);
     },
     selectAll: event => {
       onChange(!allSelected ? options.map(c => c.value) : []);
@@ -177,19 +176,17 @@ export const SelectUnstyled = ({
         key={key}
         error={!!error}
         fullWidth={fullWidth}
-        style={{ minWidth: '250px' }}
+        style={{ minWidth: '200px' }}
       >
-        <ClickAwayListener onClickAway={() => setMenuIsOpen(false)}>
-          <SizeMe noPlaceholder>
-            {({ size }) => (
-              <ReactSelect
-                {...selectProps}
-                valuesWidth={valuesWidth}
-                selectSize={size}
-              />
-            )}
-          </SizeMe>
-        </ClickAwayListener>
+        <SizeMe noPlaceholder>
+          {({ size }) => (
+            <ReactSelect
+              {...selectProps}
+              valuesWidth={valuesWidth}
+              selectSize={size}
+            />
+          )}
+        </SizeMe>
         {error && <FormHelperText>{error}</FormHelperText>}
       </FormControl>
     </div>
