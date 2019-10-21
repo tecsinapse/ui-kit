6cc839c6eca956311b904677e87f916f7f279f16
Felipe <felipefuty01@gmail.com>
2019-10-21 16:58:10 -0300

    Fix text overflox options web/mobile; Fix show selected opt

--- src/Select/Option.js
+++ src/Select/Option.js
@@ -28,7 +28,16 @@ export function Option({
       {selectProps.isMulti && (
         <Checkbox checked={isSelected} value="checkedA" />
       )}
-      {children}
+      <body1
+        style={{
+          width: '100%',
+          whiteSpace: 'normal',
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+        }}
+      >
+        {children}
+      </body1>
     </MenuItem>
   );
 }
