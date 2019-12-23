e5fe0f17d55692eec15d4c726bde6f34ed847d6a
Denner Stevens <denner.vidal@gmail.com>
2019-12-23 15:18:54 -0400

    Setup tests

---
+++ __tests__/picker.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import { DatePicker, PickersProvider } from '../src';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+  success: {
+    main: 'pink',
+  },
+});
+
+test('Render Picker', () => {
+  const selectedDate = new Date('2014-08-18T21:11:54');
+
+  const { container } = render(
+    <MuiThemeProvider theme={theme}>
+      <PickersProvider>
+        <DatePicker
+          selectedDate={selectedDate}
+          onChange={() => {}}
+          format="dd/MM/yyyy"
+          name="datepicker"
+        />
+      </PickersProvider>
+    </MuiThemeProvider>
+  );
+
+  const input = container.querySelector('input');
+  expect(input.value).toBe('18/08/2014');
+});
