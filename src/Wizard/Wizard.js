846c1f7dc1bf34ed5ab26b8f7d3a41c1e6771bfe
Denner <denner.vidal@gmail.com>
2020-05-28 11:53:31 -0400

    Removing web stepper bg

--- src/Wizard/Wizard.js
+++ src/Wizard/Wizard.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
     color: `${theme.palette.secondary.main} !important`,
     textColor: theme.palette.secondary.contrastText,
   },
-  stepper: {},
+  stepper: { backgroundColor: 'transparent' },
   wizard: {
     display: 'flex',
     flexDirection: 'column',
