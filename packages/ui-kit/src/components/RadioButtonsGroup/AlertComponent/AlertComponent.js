6ee128e108d1d9ca94b81d79c4de06fa206fea32
Wellington Oliveira <well.oliveira.snt@gmail.com>
2020-06-19 10:57:20 -0400

    Creating a new component named RadioButtonsGroup

---
+++ packages/ui-kit/src/components/RadioButtonsGroup/AlertComponent/AlertComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const style = { backgroundColor: 'red' };
+
+export const AlertComponent = ({ text }) => <div style={style}> {text}</div>;
---
+++ packages/ui-kit/src/components/RadioButtonsGroup/RadioButtonsGroup.story.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import {
+  Description,
+  Props,
+  Source,
+  Title,
+} from '@storybook/addon-docs/dist/blocks';
+import { DivFlex } from '../../withFlexCenter';
+import { GROUPS } from '../../../../../.storybook/hierarchySeparators';
+import { RadioButtonsGroup } from './RadioButtonsGroup';
+
+const style = { minWidth: '100%' };
+
+const Example = () => (
+  <div style={style}>
+    <RadioButtonsGroup />
+  </div>
+);
+
+storiesOf(`${GROUPS.FORMS}|RadioButtonsGroup`, module)
+  .addParameters({
+    component: RadioButtonsGroup,
+    docs: {
+      disable: true,
+      page: () => (
+        <>
+          <Title />
+          <Description>
+            The `RadioButtonsGroup` component can receive the following props:
+          </Description>
+          <Props />
+          <Title>Code snippets</Title>
+          <Description>
+            Here you can check the code snippet for the story.
+          </Description>
+          <Source
+            code={`
+              () => {
+                return (
+                  <RadioButtonsGroup color="primary" />
+                );
+              }
+            `}
+          />
+        </>
+      ),
+    },
+  })
+  .addDecorator(story => <DivFlex>{story()}</DivFlex>)
+  .add('RadioButtonsGroup', () => <Example />);
