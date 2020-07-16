270d776785d90528e745befa6616aa038080e533
Denner <denner.vidal@gmail.com>
2020-07-16 12:14:48 -0400

    Refactoring Wizard.d.ts

--- packages/wizard/src/Wizard/Wizard.d.ts
+++ packages/wizard/src/Wizard/Wizard.d.ts
@@ -1,27 +1,12 @@
 import { ReactNode, FC } from 'react';
 
-export type Variant = 'auto' | 'web' | 'mobile';
-
-export interface LabelsProps {
-  /** Label used to display 'Finish' button label */
-  finishText?: string;
-  /** Label used to display 'Next' button label */
-  nextText?: string;
-  /** Label used to display 'Back' button label */
-  backText?: string;
-  /** Label used to display 'Next' step on mobile */
-  nextMobileLabel?: string;
-  /** Label used to display N 'of' M on mobile */
-  stepMobileLabel?: string;
-}
-
 export interface WizardProps {
   /** Element children to be rendered */
   children: ReactNode;
   /** Active step index */
   activeStep: number;
   /** Change step handler */
-  onChange(newStep: number): string | undefined;
+  onChange: (newStep: number) => string | void;
   /** Styles object to decorate component */
   classes?: object;
   /** CSS class name applied to root div */
@@ -29,13 +14,24 @@ export interface WizardProps {
   /** Subitting loading state */
   isSubmitting?: boolean;
   /** Choose wich variant of wizard should render */
-  variant?: Variant;
+  variant?: 'auto' | 'web' | 'mobile';
   /** Size of circular stepper (mobile) */
   size?: number;
   /** Stroke width of circular stepper (mobile) */
   strokeWidth?: number;
   /** Labels used on Wizard */
-  labels?: LabelsProps;
+  labels?: {
+    /** Label used to display 'Finish' button label */
+    finishText?: string;
+    /** Label used to display 'Next' button label */
+    nextText?: string;
+    /** Label used to display 'Back' button label */
+    backText?: string;
+    /** Label used to display 'Next' step on mobile */
+    nextMobileLabel?: string;
+    /** Label used to display N 'of' M on mobile */
+    stepMobileLabel?: string;
+  };
 }
 
 declare const Wizard: FC<WizardProps>;
