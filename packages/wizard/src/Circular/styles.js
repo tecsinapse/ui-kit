873bd8718111a0923ee3ffdf8213209d60cef868
Denner <denner.vidal@gmail.com>
2020-08-28 12:11:13 -0400

    fix: removed font-family from wizard

--- packages/wizard/src/Circular/styles.js
+++ packages/wizard/src/Circular/styles.js
@@ -30,7 +30,6 @@ export const useStyles = makeStyles(({ spacing }) => ({
   circleText: {
     fontSize: '1.1rem',
     fontWeight: '500',
-    fontFamily: 'Roboto',
   },
   circularStepperText: {
     marginLeft: spacing(2),
