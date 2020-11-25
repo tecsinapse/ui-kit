ec832b9513b4b81495f93cd47a962c6357cb480c
Denner <denner.vidal@gmail.com>
2020-11-25 09:40:13 -0400

    fix: removed outline in select when disabled

--- packages/ui-kit/src/components/Select/WebComponents/SelectContainer/SelectContainer.js
+++ packages/ui-kit/src/components/Select/WebComponents/SelectContainer/SelectContainer.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { components } from 'react-select';
+import { makeStyles } from '@material-ui/styles';
 
-const style = { minWidth: '50px' };
+const useStyle = makeStyles({
+  base: {
+    minWidth: '50px',
+    '&:focus': {
+      outline: 'none',
+    },
+  },
+});
 
-export const SelectContainer = ({ children, selectProps, ...props }) => (
-  <div
-    name={selectProps.name}
-    tabIndex={Math.floor(Math.random() * 80)}
-    style={style}
-  >
-    <components.SelectContainer {...props}>
-      {children}
-    </components.SelectContainer>
-  </div>
-);
+export const SelectContainer = ({ children, selectProps, ...props }) => {
+  const classes = useStyle();
+
+  return (
+    <div
+      name={selectProps.name}
+      tabIndex={Math.floor(Math.random() * 80)}
+      className={classes.base}
+    >
+      <components.SelectContainer {...props}>
+        {children}
+      </components.SelectContainer>
+    </div>
+  );
+};
