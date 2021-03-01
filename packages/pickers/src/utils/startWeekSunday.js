a49a6ed6b152c025ab68d7a762e8f01235430b93
ryancarloscorrea <ryancarlos38@gmail.com>
2021-03-01 08:41:26 -0400

    fix: component was not rendering

--- packages/pickers/src/utils/startWeekSunday.js
+++ packages/pickers/src/utils/startWeekSunday.js
@@ -15,36 +15,4 @@ export class CustomLuxonUtils extends LuxonUtils {
   getWeekdays() {
     return this.wrapper.processWeekDayOrder();
   }
-
-  getWeekArray(date) {
-    const { index } = this.wrapper.startDay;
-    const endDate = date
-      .endOf('month')
-      .plus({ days: index !== 0 ? this.dayNames.length + 1 - index : 1 })
-      .endOf('week');
-    const startDate = date
-      .startOf('month')
-      .startOf('week')
-      .minus({ days: index !== 0 ? this.dayNames.length + 1 - index : 1 });
-
-    const { days } = endDate.diff(startDate, 'days').toObject();
-
-    const weeks = [];
-
-    new Array(Math.round(days))
-      .fill(0)
-      .map((_, i) => i)
-      .map(day => startDate.plus({ days: day }))
-      .forEach((v, i) => {
-        if (i === 0 || (i % 7 === 0 && i > 6)) {
-          weeks.push([v]);
-
-          return;
-        }
-
-        weeks[weeks.length - 1].push(v);
-      });
-
-    return weeks.filter(w => w.some(d => d.month === date.month));
-  }
 }
